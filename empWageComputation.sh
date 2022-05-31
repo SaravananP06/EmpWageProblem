20434ad767bf6d02cad0fa1b00af28669f625232
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-05-31 19:55:54 +0530

    [Saravanan]: Added Calculated Daily wage in EmpDailyWage problem

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,20 @@
 #UC1
 
 #!/bin/bash
-
+isPresent=1
+salary=0
 randomcheck=$(( $RANDOM%2 ))
-if [ $randomcheck -eq 0 ]
+if [ $randomcheck -eq $isPresent ]
 then
-	echo "Employee is Absent"
-else
 	echo "Employee is Present"
+else
+	echo "Employee is Absent"
 fi
+
+#UC2
+EmpRatePerHour=20
+empHour=8
+
+DailyWage=$(($EmpRatePerHour*$empHour))
+echo "Employee daily Wage is " $DailyWage
+
